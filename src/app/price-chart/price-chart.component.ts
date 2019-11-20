4c79457dd2883b6f00f3a1e06f08c340240150b2
Ernesto Giralt <egiralt@gmail.com>
2019-11-20 09:18:03 +0100

    Fixing overall layout / Add chart

--- src/app/price-chart/price-chart.component.ts
+++ src/app/price-chart/price-chart.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef, Output, EventEmitter } from '@angular/core';
 import { BlockItem } from '../model/block-item';
 import { ChainListenerService } from '../services/chain-listener.service';
 
-declare const Chartist: any;
+
+declare const $: any;
 declare const moment: any;
 
+const CHART_MINUTES = 3;
+const FLOAT_DIVISOR = 10000;
 
 @Component({
   selector: 'price-chart',
@@ -13,95 +16,176 @@ declare const moment: any;
 })
 export class PriceChartComponent implements OnInit {
 
+  @Output() clickPoint = new EventEmitter()
   private _blockList;
+  private _dataItems: number[];
+  private _labelItems: number[];
+  private _serie;
 
-  @Input()
-  set blockList(value: BlockItem[]) {
-    this._blockList = value;
-    this.generateChart();
-  }
+  private plot;
 
-  get blockList(): BlockItem[] {
-    return this._blockList;
-  }
+  /**
+   * All the options used by the chart library
+   */
+  private chartOptions = {
+    series: {
+      lines: { show: true },
+      points: { show: false }
+    },
+    selection: {
+      mode: "x"
+    },
+    grid: {
+      hoverable: true,
+      clickable: true,
+      markings: this.weekendAreas
+    },
+    // xaxis: {
+    //   mode: "time",
+    //   timeBase: "miliseconds",
+    //   tickLength: 1,
+    //   gridLines: false
+    // },
+    // zoom: {
+    // 	interactive: true
+    // },
+    pan: {
+      interactive: true,
+      enableTouch: true
+    }
+  };
 
-  private chart;
-
-  constructor(private chainService: ChainListenerService) {
-    // create 10 fake points to initialize the graph
-    // for (var i = 0; i <= 9; i++) {
-    //   this.blockList.push ()
-    // }
+  constructor(private chainService: ChainListenerService, private changeRef: ChangeDetectorRef) {
+    this._dataItems = Array(CHART_MINUTES * 60).join(0).split('').map(a => parseFloat(a))
+    this._labelItems = Array(CHART_MINUTES * 60).join(0).split('')
   }
 
   ngOnInit() {
-    this.chainService.BlockEvent.subscribe(block => {
-
-    })
-    this.generateChart();
-    // // Update the chart every time it's created with a delay of 1.5 seconds
-    // this.chart.on('created', function () {
-    //   if (window.__anim0987432598723) {
-    //     clearTimeout(window.__anim0987432598723);
-    //     window.__anim0987432598723 = null;
-    //   }
-    //   window.__anim0987432598723 = setTimeout(chart.update.bind(chart), 1500);
-    // });
+    this.initializeChart();
+    this.chainService.BlockEvent.subscribe(block => this.renderChart(block));
   }
 
-  generateChart() {
-    if (this.blockList && this.blockList.length > 0) {
-      // Our labels and three data series
-      var labels = [];
-      var seriesData = [];
-      this.blockList.forEach(item => {
-        // X labels
-        var timeSt = moment(item.timestamp * 1000).format('YY-MM-DD, HH:mm:ss');
-        labels.push(timeSt);
-        // Series
-        seriesData.push(item.priceIndex);
-      });
+  /**
+   * Called by the event of chain. Get the new block and redraw the chart
+   * @param block 
+   */
+  renderChart(block: BlockItem) {
+    this._dataItems.shift();
+    this._dataItems.push(block.priceIndex); // To improve the precision
 
-      console.log(seriesData);
-      console.log(labels);
+    this._labelItems.shift();
+    this._labelItems.push(block.timestamp);
+    // this._labelItems.push(moment(block.timestamp * 1000).format("YYY-MM-DD HH:mm"));
 
-      var data = {
-        labels: labels,
-        series: {
-          name: 'index',
-          data: seriesData
-        }
-      };
-
-      // We are setting a few options for our chart and override the defaults
-      var options = {
-        // Don't draw the line chart points
-        showPoint: true,
-        // Disable line smoothing
-        lineSmooth: true,
-        // X-Axis specific configuration
-        axisX: {
-          // We can disable the grid for this axis
-          showGrid: true,
-          // and also don't show the label
-          showLabel: true
-        },
-        // Y-Axis specific configuration
-        axisY: {
-          // Lets offset the chart a bit from the labels
-          offset: 60,
-          // The label interpolation function enables you to modify the values
-          // used for the labels on each axis. Here we are converting the
-          // values into million pound.
-          labelInterpolationFnc: function (value) {
-            return '$ ' + value;
-          }
-        }
-      };
-
-      // All you need to do is pass your configuration as third parameter to the chart function
-      new Chartist.Line('.ct-chart', data, options);
+    // Build the series with the expected layout
+    var serie = [];
+    for (var i = 0; i < this._dataItems.length; i++) {
+      serie.push([i, this._dataItems[i]]);
     }
+
+    // Redraw all
+    this.plot = $.plot("#placeholder", [{
+      data: serie,
+      label: "Price",
+      lines: { show: true, fill: true }
+    }],
+      this.chartOptions);
+
+    this.changeRef.markForCheck();
+  }
+
+  /**
+   * Helper function to draw the weekends in the chart. Called for the chart
+   */
+  weekendAreas(axes) {
+
+    var markings = [],
+      d = new Date(axes.xaxis.min);
+
+    // go to the first Saturday
+    d.setUTCDate(d.getUTCDate() - ((d.getUTCDay() + 1) % 7))
+    d.setUTCSeconds(0);
+    d.setUTCMinutes(0);
+    d.setUTCHours(0);
+
+    var i = d.getTime();
+
+    // when we don't set yaxis, the rectangle automatically
+    // extends to infinity upwards and downwards
+    do {
+      markings.push({ xaxis: { from: i, to: i + 2 * 24 * 60 * 60 * 1000 } });
+      i += 7 * 24 * 60 * 60 * 1000;
+    } while (i < axes.xaxis.max);
+
+    return markings;
+  }
+
+  /**
+   * Initialize the canvas and set the events 
+   */
+  initializeChart() {
+    this.plot = $.plot("#placeholder", [{
+      data: [],
+      label: "Price",
+      lines: { show: true, fill: true }
+    }], this.chartOptions);
+
+    // Insert a div dynamically into the outest element
+    // $("<div id='tooltip'></div>").appendTo("body");
+
+    var el = $("#placeholder");
+    el.bind("plotselected", (event, ranges) => {
+
+      // do the zooming
+      $.each(this.plot.getXAxes(), function (_, axis) {
+        var opts = axis.options;
+        opts.min = ranges.xaxis.from;
+        opts.max = ranges.xaxis.to;
+      });
+      this.plot.setupGrid();
+      this.plot.draw();
+      this.plot.clearSelection();
+
+      // don't fire event on the overview to prevent eternal loop
+
+      //overview.setSelection(ranges, true);
+    });
+    // Hovering over the grid
+    el.bind("plothover", (event, pos, item) => {
+
+      if (!pos.x || !pos.y) {
+        return;
+      }
+
+      if (item) {
+        var x = item.datapoint[0], y = item.datapoint[1].toFixed(5);
+        if (y > 0) {
+          var timeLabel = moment(this._labelItems[x] * 1000).format('YYYY-MM-DD, HH:mm:ss')
+          var tooltipText = item.series.label + ": <strong>" + y + "</strong> at " + timeLabel;
+
+          $("#chart-tooltip").html(tooltipText)
+            .css({ top: item.pageY + 5, left: item.pageX + 5 })
+            .fadeIn(100);
+        } else {
+          $("#chart-tooltip").hide();
+        }
+      } else {
+        $("#chart-tooltip").hide();
+      }
+    });
+
+    // When the mouse is moved, the tooltips will hide 
+    el.bind("plothovercleanup", (event, pos, item) => {
+      $("#chart-tooltip").hide();
+    });
+
+    // Click on the grid
+    el.bind("plotclick", (event, pos, item) => {
+      if (item) {
+        this.plot.highlight(item.series, item.datapoint);
+        console.log(item);
+      }
+    });
   }
 
 }
